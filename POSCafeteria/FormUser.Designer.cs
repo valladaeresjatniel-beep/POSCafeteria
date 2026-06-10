2ad0c33d7e584a1d3e81fb4f3160487836f9b8ca
Jatniel_VT <Valladaeresjatniel@gmail.com>
2026-06-09 20:54:51 -0600

    Form user

--- POSCafeteria/FormUser.Designer.cs
+++ POSCafeteria/FormUser.Designer.cs
@@ -74,7 +74,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(130, 101);
+            label2.Location = new Point(127, 101);
             label2.Name = "label2";
             label2.Size = new Size(316, 25);
             label2.TabIndex = 2;
